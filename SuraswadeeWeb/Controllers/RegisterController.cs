3a4ab6055a3b1df54a1db419806827c0bb9e56db
Perawatt <pekung4433@perfenterprise.com>
2016-01-28 17:34:13 +0700

    Implememt UI Register

---
+++ SuraswadeeWeb/Controllers/RegisterController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuraswadeeWeb.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult RegisterCompleted()
+        {
+            return View();
+        }
+    }
+}
