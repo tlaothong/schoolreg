16864599ffc50f84038485cff5ef34db83e3e7f2
sirinarin <sirinarin@perfenterprise.com>
2016-02-02 14:21:01 +0700

    xoxo

--- SuraswadeeWeb/Controllers/RegisterController.cs
+++ SuraswadeeWeb/Controllers/RegisterController.cs
@@ -52,17 +52,26 @@ namespace SuraswadeeWeb.Controllers
 
         public string Upload(HttpPostedFileBase file)
         {
-            if (file !=null)
+            try
             {
-                var fileName = Guid.NewGuid().ToString() + ".jpg";
-                var fileURL = System.Web.HttpContext.Current.Server.MapPath("~/ImgUpload/Img/" + fileName);
-                file.SaveAs(fileURL);
-                return "/ImgUpload/Img/" + fileName;
+                if (file != null)
+                {
+                    var fileName = Guid.NewGuid().ToString() + ".jpg";
+                    var fileURL = System.Web.HttpContext.Current.Server.MapPath("~/ImgUpload/Img/" + fileName);
+                    file.SaveAs(fileURL);
+                    return "/ImgUpload/Img/" + fileName;
+                }
+                else
+                {
+                    return null;
+                }
             }
-            else
+            catch (Exception e)
             {
-                return null;
+
+                throw e;
             }
+          
           
         }
     }
